7de31352d1dfe4bff431564857378f6c7cd43601
momoNiti <60070041@kmitl.ac.th>
2019-02-04 10:43:52 +0700

    add new widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,103 +9,118 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(), //home คือตัว class แรกที่เอามาแสดงผล
     );
   }
 }
+// วิธีการรันผ่าน cmd
+// flutter emulators จะได้ ชื่อ device มา จากนั้นก็ flutter emulators --launch ตามด้วยชื่อ device ที่โชว์
+// พอจะรันโค้ดก็ flutter run
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+//Stateless รัน widget ทีเดียวเลิก ใช้ memory น้อยกว่า
+// Statefull เก็บข้อมูล และมีการ notified ด้วย ถ้าจะเขียนต้องมีสองคลาส
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+//ลองใชเ stateless ทำปุ่มบวกเลข ที่เลขไม่เพิ่มเพราะมันเป็น stateless
+// class MyHomePage extends StatelessWidget {
+//   int counter = 0;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Hello Wold : title"),
+//       ), // AppBar เป็นส่วนหัว
+//       body: Text("Counter = ${counter} "),
+//       floatingActionButton: IconButton(
+//         icon: Icon(Icons.add),
+//         onPressed: () {
+//           counter++;
+//           print("counter value = ${counter}");
+//         },
+//       ),
+//     ); //scaffold คือ widget พร้อมใช้
+//   }
+// }
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+// ลองใช้ statful สร้างปุ่มกดดได้
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() {
+//     return MyHomePageState(); // create state ให้ส่งไปทื class state ตัวลูก
+//   }
+// }
+// class MyHomePageState extends State<MyHomePage> {
+//   //ตอน extends ให้ reference class แม่
+//   int counter = 0;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Hello Wold : title"),
+//       ), // AppBar เป็นส่วนหัว
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.spaceAround, //mainAxis จัดในแนวแกน X
+//         crossAxisAlignment: CrossAxisAlignment.center, //crossAxis จัดนในแนวแกน Y
+//         children: <Widget>[
+//           Text("Counter = ${counter}"),
+//           Text("Test row2"),
+//           Text("Text row3"),
+//         ],
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: Icon(Icons.add), //.add คือเป็น + ไปดู icon อื่นๆได้ใน material icon
+//         onPressed: () {
+//           setState(() {
+//             //Stateful ต้อง set state value ที่เราจะต้องเปลี่ยนแปลง
+//             counter++;
+//           });
+//           print("counter value = ${counter}");
+//         },
+//       ),
+//     ); //scaffold
+//   }
+// }
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+// ทำ tabbar
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Title tabbar"),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.add),
+                text: "add",
+              ),
+              Tab(
+                icon: Icon(Icons.alarm),
+                text: "alarm",
+              ),
+              Tab(
+                icon: Icon(Icons.build),
+                text: "build",
+              ),
+              Tab(
+                icon: Icon(Icons.brush),
+                text: "Brush",
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            Icon(Icons.add),
+            Icon(Icons.access_alarms),
+            Icon(Icons.build),
+            Icon(Icons.brush),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
