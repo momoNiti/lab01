fef1923a91904812fa96e87a778602268aaf86a9
momoNiti <60070041@kmitl.ac.th>
2019-02-18 09:37:21 +0700

    dropdown

--- lib/ui/first_screen.dart
+++ lib/ui/first_screen.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:lab01/ui/detail_screen.dart';
 
 //ทำ drop down ใช้ stateful
-class FirstScreen extends StatefulWidget{
+class FirstScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return FirstScreenState();
   }
-  
 }
-class FirstScreenState extends State<FirstScreen>{
+
+class FirstScreenState extends State<FirstScreen> {
   List<String> _passengers = <String>["", "1", "2", "3", "4", "5"];
+  String _passenger = "1";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,15 +22,13 @@ class FirstScreenState extends State<FirstScreen>{
         children: <Widget>[
           InputDecorator(
             decoration: InputDecoration(
-              icon: Icon(Icons.people),
-              labelText: "passenger"
-            ),
-            isEmpty: true,
+                icon: Icon(Icons.people), labelText: "passenger"),
+            isEmpty: _passenger == "", //แก้ปัญหาเรื่อง label กับ value ทับกัน
             child: DropdownButtonHideUnderline(
               child: DropdownButton(
-                onChanged: (String value) {
-                  
-                },
+                value: _passenger, //เลือกค่าที่จะโชว์เป็นค่าแรก
+                isDense: true, // ทำให้มันบีบให้สวยงาม
+                onChanged: (String value) {},
                 items: _passengers.map((String value) {
                   return DropdownMenuItem<String>(
                     value: value,
